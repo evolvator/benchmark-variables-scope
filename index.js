4bb1b5c68a81174c5b7e91310bb0951ea2a81ee3
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-06-25 11:32:43 +0000

    code

--- index.js
+++ index.js
@@ -1,20 +1,222 @@
 var Benchmark = require('benchmark');
 var tb = require('travis-benchmark');
 var _ = require('lodash');
-var suite = new Benchmark.Suite(`variants of actions`);
+var global = require('global');
 
-var f = function() {};
-var functions = {};
-for (var i = 0; i < 1000; i++) {
-  functions[i] = f;
-}
+var suite = new Benchmark.Suite(`variables scope`);
 
-eval(`suite.add('switch', function() { var s = '999'; switch (s) { ${_.map(functions, function(v, k) { return `case '${k}': return f();`; }).join('')} } });`);
-eval(`suite.add('if', function() { var s = '999'; ${_.map(functions, function(v, k) { return `if (s == '${k}') return f();`; }).join('')} });`);
-suite.add('hash-of-functions', function() {
-  var s = '999';
-  functions[s]();
-});
+(function() {
+  var a = {
+    c: 123,
+    b: function() {
+      this.c;
+    }
+  };
+  suite.add('object this', function() {
+    return a.b();
+  });
+})();
+
+(function() {
+  var x;
+  global.y = 123;
+  x = function() {
+    return this.y;
+  };
+  suite.add('global this', function() {
+    return x();
+  });
+})();
+
+(function() {
+  var x, y = 123;
+  x = function() {
+    return y;
+  };
+  suite.add('scope 1', function() {
+    x();
+  });
+})();
+
+(function() {
+  var x, y = 123;;
+  (function() {
+    x = function() {
+      return y;
+    };
+  })();
+  suite.add('scope 2', function() {
+    x();
+  });
+})();
+
+(function() {
+  var x, y = 123;;
+  (function() {
+    (function() {
+      x = function() {
+        return y;
+      };
+    })();
+  })();
+  suite.add('scope 3', function() {
+    x();
+  });
+})();
+
+(function() {
+  var x, y = 123;;
+  (function() {
+    (function() {
+      (function() {
+        x = function() {
+          return y;
+        };
+      })();
+    })();
+  })();
+  suite.add('scope 4', function() {
+    x();
+  });
+})();
+
+(function() {
+  var x, y = 123;;
+  (function() {
+    (function() {
+      (function() {
+        (function() {
+          x = function() {
+            return y;
+          };
+        })();
+      })();
+    })();
+  })();
+  suite.add('scope 5', function() {
+    x();
+  });
+})();
+
+(function() {
+  var x, y = 123;;
+  (function() {
+    (function() {
+      (function() {
+        (function() {
+          (function() {
+            x = function() {
+              return y;
+            };
+          })();
+        })();
+      })();
+    })();
+  })();
+  suite.add('scope 6', function() {
+    x();
+  });
+})();
+
+(function() {
+  var x, y = 123;;
+  (function() {
+    (function() {
+      (function() {
+        (function() {
+          (function() {
+            (function() {
+              x = function() {
+                return y;
+              };
+            })();
+          })();
+        })();
+      })();
+    })();
+  })();
+  suite.add('scope 7', function() {
+    x();
+  });
+})();
+
+(function() {
+  var x, y = 123;;
+  (function() {
+    (function() {
+      (function() {
+        (function() {
+          (function() {
+            (function() {
+              (function() {
+                x = function() {
+                  return y;
+                };
+              })();
+            })();
+          })();
+        })();
+      })();
+    })();
+  })();
+  suite.add('scope 8', function() {
+    x();
+  });
+})();
+
+(function() {
+  var x, y = 123;;
+  (function() {
+    (function() {
+      (function() {
+        (function() {
+          (function() {
+            (function() {
+              (function() {
+                (function() {
+                  x = function() {
+                    return y;
+                  };
+                })();
+              })();
+            })();
+          })();
+        })();
+      })();
+    })();
+  })();
+  suite.add('scope 9', function() {
+    x();
+  });
+})();
+
+(function() {
+  var x, y = 123;;
+  (function() {
+    (function() {
+      (function() {
+        (function() {
+          (function() {
+            (function() {
+              (function() {
+                (function() {
+                  (function() {
+                    x = function() {
+                      return y;
+                    };
+                  })();
+                })();
+              })();
+            })();
+          })();
+        })();
+      })();
+    })();
+  })();
+  suite.add('scope 10', function() {
+    x();
+  });
+})();
 
 tb.wrapSuite(suite);
 suite.run({ async: true });
